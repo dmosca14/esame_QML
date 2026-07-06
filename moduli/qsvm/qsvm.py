b98be6e3f0d990adf322934c7955ef9e1478b7b2
Davide Petillo <67431890+hu-sh@users.noreply.github.com>
2026-07-06 17:42:18 +0200

    Update qsvm.py

--- moduli/qsvm/qsvm.py
+++ moduli/qsvm/qsvm.py
@@ -4,9 +4,9 @@ from .addestramento import addestramento
 from .visualizzazione import test_e_visualizzazione
 
 def costruisci_qsvm(train_set_raw, test_set_raw, numero_features, modulo_encoding, iperparametri_quantistici, iperparametri_classici, nome_dataset=""):
-
+    
     # Creazione della cartella relativa alla run attuale.
-
+    
     nome_encoding = getattr(modulo_encoding, "nome_encoding")
     nome_pulito_encoding = nome_encoding.replace(" ", "_").lower() 
    
@@ -16,19 +16,18 @@ def costruisci_qsvm(train_set_raw, test_set_raw, numero_features, modulo_encodin
     percorso_finale = os.path.join(cartella_run_attuale, nome_cartella_encoding)
     os.makedirs(percorso_finale, exist_ok = True)
 
-    # Costruzione della QSVM.
-    
+    print(f"QUANTUM SUPPORT VECTOR MACHINE PER {nome_encoding.upper()}")
     print(f"\nDestinazione output: {percorso_finale}")
 
     # Addestramento e validazione della QSVM.
-
-    set_adattato, matrici_gram, modello, storico = addestramento(
+    
+    set_adattato, matrici_gram, modello, storico, migliori_ip_quantistici, migliori_ip_classici = addestramento(
         nome_encoding, train_set_raw, test_set_raw, numero_features, modulo_encoding, 
         iperparametri_quantistici, iperparametri_classici, percorso_finale, nome_dataset
     )
 
-    # Test della miglior QSVM e visualizzazione dei risultati.
-
+    # Test della miglior QSVM e visualizzazione dei risultati
+    
     test_e_visualizzazione(nome_encoding, set_adattato, matrici_gram, modello, percorso_finale, nome_dataset)
 
-    return modello, set_adattato, matrici_gram
+    return modello, set_adattato, matrici_gram, storico, migliori_ip_quantistici, migliori_ip_classici
